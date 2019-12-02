9c3a7feaecfee057f7ceb93ac120bf344904f516
Luidy Vaccaro <luidy@upoutsourcing.com>
2019-12-02 15:07:53 -0300

    lesson done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [repositories, setRespositories] = useState([]);
+  const [location, setLocation] = useState({});
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch('https://api.github.com/users/luidyvcc/repos');
+      const data = await response.json();
+      setRespositories(data);
+    }
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    const filtered = repositories.filter(repo => repo.favorite);
+    document.title = `Você tem ${filtered.length}`;
+  }, [repositories]);
+
+  useEffect(() => {
+    const watchId = navigator.geolocation.watchPosition(fandlePositionReceived);
+    return () => navigator.geolocation.clearWatch(watchId);
+  }, []);
+
+  const handleFavorite = id => {
+    const newRepositories = repositories.map(repo => {
+      return repo.id === id ? { ...repo, favorite: !repo.favorite } : repo
+    })
+    setRespositories(newRepositories);
+  }
+
+  const fandlePositionReceived = ({ coords }) => {
+    const { latitude, longitude } = coords;
+    setLocation({ latitude, longitude });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      Latitude: {location.latitude}<br />
+      Longitude: {location.longitude}<br />
+      <hr />
+      <ul>
+        {repositories.map(repo => (
+          <li key={repo.id}>
+            <button onClick={() => handleFavorite(repo.id)}>Favoritar</button>
+            {repo.favorite && <strong> * </strong>}
+            {repo.name}
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
